48606f834574f2b070599179c8887ffe0efb2799
Ayoubxy <168744709+Ayoubxy@users.noreply.github.com>
2025-06-25 15:38:59 +0200

    Exceptions

--- Peregos.py
+++ Peregos.py
@@ -39,7 +39,7 @@ def empfange_nachrichten():
         channel.basic_consume(queue="queue_peregos", on_message_callback=callback, auto_ack=True)
         channel.start_consuming()
     except Exception as e:
-        text_area.insert(tk.END, f"\n[Connection error]: {e}\n")
+        text_area.insert(tk.END, f"\n[Connection error], [Rabbit MQ not available, please try again in a few minutes]: {e}\n")
 
 # Starte den Empfangs-Thread
 thread = Thread(target=empfange_nachrichten, daemon=True)
